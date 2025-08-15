118c023b9fc9b47fdf43c8bd601c12d8c56a6d8f
Ilias Chrysogelos <125901761+iliaschr@users.noreply.github.com>
2025-08-15 23:49:27 +0300

    Init

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <header>
+            <h1>Ilias Chrysogelos</h1>
+            <nav>
+                <a href="#about">About</a>
+                <a href="#projects">Projects</a>
+                <a href="#contact">Contact</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
